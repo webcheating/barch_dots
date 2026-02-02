8399790422ac8c7f33e0e821f36f90cd9379cd2c
webcheating <swag.designer812@passfwd.com>
2026-02-02 21:47:05 +0300

    :)

---
+++ pc/root/nvim/init.lua
@@ -0,0 +1,9 @@
+require("keymaps")
+require("options")
+require("plugins.lazy")
+require("plugins.keymaps")
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
