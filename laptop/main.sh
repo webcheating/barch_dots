a2a69f4dc101654c4dd4d5bef61df2d81e4d9b68
webcheating <swag.designer812@passfwd.com>
2026-02-02 23:41:33 +0300

    :333

--- laptop/main.sh
+++ laptop/main.sh
@@ -10,13 +10,13 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
-error() { echo -e "${RED}✗ $1${NC}"; }
-success() { echo -e "${GREEN}✓ $1${NC}"; }
-warning() { echo -e "${YELLOW}⚠ $1${NC}"; }
+error() { echo -e "${RED}[x] $1${NC}"; }
+success() { echo -e "${GREEN}[+] $1${NC}"; }
+warning() { echo -e "${YELLOW}[!] $1${NC}"; }
 
 printf "[*] installing dotfiles...\n\n"
 
-sudo pacman -Syu --needed base-devel git rsync
+sudo pacman -Syu --needed base-devel git
 
 if ! command -v yay &> /dev/null && ! command -v paru &> /dev/null; then
     echo "[*] installing aur"
@@ -33,7 +33,7 @@ mkdir -p $bck_dir
 
 echo '[*] installing packages'
 #yay -Syuv obmenu-generator lxappearance-obconf mousepad feh xorg-xinput flameshot tint2 thunar
-yay -S --noconfirm --needed obmenu-generator lxappearance mousepad feh xorg-xinput flameshot tint2 thunar nvim direnv
+yay -S --noconfirm --needed obmenu-generator lxappearance mousepad feh xorg-xinput flameshot tint2 thunar nvim direnv rsync alacritty upowet xorg-xset acpi
 
 echo '[*] installing fonts...'
 yay -S --noconfirm --needed ttf-jetbrains-mono ttf-jetbrains-mono-nerd terminus-font-ttf ttf-dejavu ttf-droid ttf-liberation ttf-apple-emoji ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-extra
@@ -57,7 +57,8 @@ sudo mv /usr/share/icons /usr/share/bck.icons && sudo mkdir /usr/share/icons && 
 [ -f "x/xprofile" ] && sudo cp x/xprofile /etc/
 [ -f "x/.xinitrc" ] && cp x/.xinitrc ~/
 [ -f "x/.xserverrc" ] && cp x/.xserverrc ~/
-
+[ -f ".tmux" ] && cp -r .tmux ~/
+[ -f ".tmux.conf" ] && cp .tmux.conf ~/
 
 echo '[*] setting up .config'
 if [ -d "config" ]; then
@@ -81,5 +82,5 @@ echo '[*] setting up wallpapers...'
 sudo mkdir -p /usr/share/wallpapers/blackarch-artwork && sudo cp wallpapers/wallpaper-fog.jpg /usr/share/wallpapers/blackarch-artwork/
 
 echo '=========================================================================='
-printf "\n\n[*] all done. don't forget to manualy configure lxappearance ^_^\n"
+printf "[*] all done. don't forget to manualy configure lxappearance ^_^\n"
 echo '=========================================================================='
