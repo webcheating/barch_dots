1d18cf0dfd48798f9e15681dcdd073e8856be44f
webcheating <swag.designer812@passfwd.com>
2026-02-23 00:24:35 +0300

    removed unnecessary verbose output for copyign icons & themes

--- laptop/new.openbox/main.sh
+++ laptop/new.openbox/main.sh
@@ -162,7 +162,7 @@ sudo git clone https://github.com/webcheating/nvim /root/.config/nvim/ && succes
 #[ -d "../../global_files/nvim" ] && sudo rsync -av --progress ../../global_files/nvim /root/.config/ && success "nvim config for root copied"
 
 echo "[*] installing icons and themes"
-[ -d "usr_share" ] && sudo rsync -av --progress usr_share/ /usr/share/ && success "icons and themes copied"
+[ -d "usr_share" ] && sudo rsync -av usr_share/ /usr/share/ && success "icons and themes copied"
 
 echo ""
 echo "================================"
